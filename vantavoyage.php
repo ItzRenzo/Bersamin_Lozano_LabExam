343b0c030b38b154de41fa55be52497be395ea3b
ItzRenzo <6thgen15@gmail.com>
2025-09-26 15:16:47 +0800

    Remove session check for logged-in users on landing page

--- vantavoyage.php
+++ vantavoyage.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header('Location: login.php');
-    exit;
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
